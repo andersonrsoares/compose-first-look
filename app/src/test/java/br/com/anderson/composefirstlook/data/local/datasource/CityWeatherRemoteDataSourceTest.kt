722a01748caf5679af7879b66939da5af313d974
Anderson Soares <anderson.r.soares@gmail.com>
2022-06-19 12:49:26 +0100

    removed tedt

--- app/src/test/java/br/com/anderson/composefirstlook/data/local/datasource/CityWeatherRemoteDataSourceTest.kt
+++ app/src/test/java/br/com/anderson/composefirstlook/data/local/datasource/CityWeatherRemoteDataSourceTest.kt
@@ -2,7 +2,7 @@ package br.com.anderson.composefirstlook.data.local.datasource
 
 import org.junit.Test
 
-@Test
-class CityWeatherRemoteDataSourceTest {
-
-}
+//@Test
+//class CityWeatherRemoteDataSourceTest {
+//
+//}
